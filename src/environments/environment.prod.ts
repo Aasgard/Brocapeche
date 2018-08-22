2ef2334769717df5fe4df87728ab1feed5c73974
François-Régis LANCIEN <francois.regis.lancien@gmail.com>
2018-08-22 15:08:27 +0200

    Removed conf for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-    production: true,
-    firebase: {
-        apiKey: 'AIzaSyDMYE3fVlDSb8XTBN4aGTMV5NWbu38VhJc',
-        authDomain: 'brocapeche.firebaseapp.com',
-        databaseURL: 'https://brocapeche.firebaseio.com',
-        projectId: 'brocapeche',
-        storageBucket: 'brocapeche.appspot.com',
-        messagingSenderId: '903753794860'
-    }
+    production: true
 };
